39f401780b9964eef0945c426ac6ccc2af2a7ec9
Hupewolf <nhannguyen.ix5@gmail.com>
2025-11-29 10:30:30 +0700

    challenge4 js calculator fix

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 function calc(test) {
-    const n1 = Number(document.querySelector(".num1").value);
-    const n2 = Number(document.querySelector(".num2").value);
+    const n1 = parseFloat(document.querySelector(".num1").value);
+    const n2 = parseFloat(document.querySelector(".num2").value);
     const kqBox = document.querySelector(".kq");
 
-    if (isNaN(n1) || isNaN(n2) || n1.trim() === "" || n2.trim() === "") {
+    if (isNaN(n1) || isNaN(n2)) {
         kqBox.innerHTML = "không đủ dữ kiện";
         kqBox.style.color = "red";
         return;
@@ -24,6 +24,6 @@ function calc(test) {
         case "/": kq = n1 / n2; break;
     }
 
-    kqBox.innerHTML = `Kết quả: ${Number(kq.toFixed(10))}`;
+    kqBox.innerHTML = `Kết quả: ${parseFloat(kq.toFixed(10))}`;
     kqBox.style.color = "green";
 }
